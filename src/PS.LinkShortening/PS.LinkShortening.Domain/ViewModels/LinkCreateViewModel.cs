5026bd009583d677217926b1a1ffa0a90e24a401
Aliaksandr Kudrautsau <sany199574@gmail.com>
2022-09-12 21:12:36 +0300

    Добавлен View model:  - LinkCreateViewModel

---
+++ src/PS.LinkShortening/PS.LinkShortening.Domain/ViewModels/LinkCreateViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PS.LinkShortening.Domain.ViewModels
+{
+    public class LinkCreateViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string LongURL { get; set; } = null!;
+    }
+}

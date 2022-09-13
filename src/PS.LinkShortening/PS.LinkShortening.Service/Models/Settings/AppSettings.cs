68f940637ff1aa5486f72605e974dec0e993d608
Aliaksandr Kudrautsau <sany199574@gmail.com>
2022-09-13 07:21:05 +0300

    Добавил общии настройки для конфигурации AppSettings

--- src/PS.LinkShortening/PS.LinkShortening.Service/Models/Settings/AppSettings.cs
+++ src/PS.LinkShortening/PS.LinkShortening.Service/Models/Settings/AppSettings.cs
@@ -2,5 +2,10 @@
 {
     public class AppSettings
     {
+        public UrlSettings Url { get; set; } = null!;
+        public CacheSettings Cache { get; set; } = null!;
+        public GoogleSettings Google { get; set; } = null!;
+        public DatabaseSettings Database { get; set; } = null!;
+        public SecuritySettings Security { get; set; } = null!;
     }
 }

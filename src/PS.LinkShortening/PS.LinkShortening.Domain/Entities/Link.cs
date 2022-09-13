cb96b04b83c7b068cac5545fa72e286f5c8d76bc
Aliaksandr Kudrautsau <sany199574@gmail.com>
2022-09-13 07:07:43 +0300

    Изменил сущность Link

--- src/PS.LinkShortening/PS.LinkShortening.Domain/Entities/Link.cs
+++ src/PS.LinkShortening/PS.LinkShortening.Domain/Entities/Link.cs
@@ -1,15 +1,28 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace PS.LinkShortening.Domain.Entities
 {
+    [Index(propertyNames: nameof(Key), IsUnique = true)]
     public class Link
     {
+        [Key]
         public int Id { get; set; }
 
         [Required]
-        public string LongURL { get; set; } = null!;
-        public string? ShortURL { get; set; } = null!;
+        [MaxLength(10)]
+        public string Key { get; set; } = null!;
+
+        [Required]
+        [MaxLength(512)]
+        public string? Url { get; set; } = null!;
+        public string? LongURL { get; set; } = null!;
+
         public DateTime DateCreated { get; set; }
+        public DateTime? Expires { get; set; }
+
+        public string? Metadata { get; set; }
         public int CountTransitions { get; set; }
 
     }

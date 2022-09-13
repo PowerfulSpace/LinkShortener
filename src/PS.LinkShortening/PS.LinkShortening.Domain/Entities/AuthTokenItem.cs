8767acdc8c485dbb468697e1da01199b086335eb
Aliaksandr Kudrautsau <sany199574@gmail.com>
2022-09-13 07:09:40 +0300

    Добавлена сущность AuthTokenItem

---
+++ src/PS.LinkShortening/PS.LinkShortening.Domain/Entities/AuthTokenItem.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace PS.LinkShortening.Domain.Entities
+{
+    [Index(nameof(AuthToken))]
+    public class AuthTokenItem
+    {
+        [Key]
+        public int Id { get; set; }
+        public string AuthToken { get; set; } = null!;
+        public bool CanCreate { get; set; }
+        public bool CanGet { get; set; }
+    }
+}
